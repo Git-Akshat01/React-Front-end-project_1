4a5853fc9dc49fd807428c0b268fcb5ba0e367c6
Akshat Tiwari <akshattiwari0901@gmail.com>
2024-02-26 14:13:37 +0530

    All files of the react app project completed.

---
+++ src/components/Footer.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <h1>TechyStar</h1>
+        <p>@all right reserved</p>
+      </div>
+
+      <div>
+        <h5>Follow Us</h5>
+        <div>
+          <a
+            href="https://www.linkedin.com/in/akshat-tiwari-18a5a2192/
+"
+            target={"blank"}
+          >
+            LinkedIn
+          </a>
+          <a href="https://www.youtube.com/@Akshat_79" target={"blank"}>
+            Youtube
+          </a>
+
+          <a href="https://www.instagram.com/akshat.tiwari.7/" target={"blank"}>
+            Instagram
+          </a>
+
+          <a href="https://github.com/Git-Akshat01" target={"blank"}>
+            Github
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
